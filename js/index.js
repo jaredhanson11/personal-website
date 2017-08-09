c6bebefa60e7fab87763bf07998eaada4881b3af
Jared Hanson <jared_t_hanson@apple.com>
2017-08-09 16:53:54 -0700

    Finish portfolio, added experience & footer

--- js/index.js
+++ js/index.js
@@ -39,10 +39,21 @@ function introScreen() {
   function afterAnimation() {
     var navBar = $('.nav-bar');
     navBar.slideDown();
+    unlockTop();
+  }
+
+  function lockToTop() {
+    $('body').scrollTop(0);
+    $('body').css('overflow', 'hidden');
+  }
+
+  function unlockTop() {
+      $('body').css('overflow', '');
   }
   /// End Helper functions
-
   var introText = 'Hi, I\'m Jared.';
+
+  lockToTop();
   blink('.intro-text > .caret', 0, 1, function(){
     typeNext('.intro-text > .intro-message', '', introText, function() {
       blink('.intro-text > .caret');
